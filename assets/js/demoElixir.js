892d819dd23f8b32e4f4d72b3d9d57601277099b
Juan Carlos Lamas Alfaro <juank.yea@hotmail.com>
2021-09-26 16:00:06 -0300

    feat: style and js stuff

--- assets/js/demoElixir.js
+++ assets/js/demoElixir.js
@@ -1,31 +1,33 @@
+
+const listenForChats = channel => {
+  document.getElementById('chat-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    const userName = document.getElementById('user-name').value;
+    const userMsg = document.getElementById('user-msg').value;
+
+    if (!!!userName || !!!userMsg) return;
+
+    channel.push('shout', { name: userName, body: userMsg });
+
+    document.getElementById('user-msg').value = '';
+  });
+
+  channel.on('shout', payload => {
+    const chatBox = document.querySelector('#chat-box');
+    const msgBlock = document.createElement('p');
+
+    msgBlock.insertAdjacentHTML('beforeend', `<b>${payload.name}:</b> ${payload.body}`);
+    chatBox.appendChild(msgBlock);
+  });
+};
+
 const DemoElixir = {
-  init(socket) {
-    const channel = socket.channel('main:lobby', {})
-    channel.join()
-    this.listenForChats(channel)
-  },
-
-  listenForChats(channel) {
-    document.getElementById('chat-form').addEventListener('submit', e => {
-      e.preventDefault()
-
-      const userName = document.getElementById('user-name').value
-      const userMsg = document.getElementById('user-msg').value
-
-      channel.push('shout', { name: userName, body: userMsg })
-
-      document.getElementById('user-name').value = ''
-      document.getElementById('user-msg').value = ''
-    })
-
-    channel.on('shout', payload => {
-      const chatBox = document.querySelector('#chat-box')
-      const msgBlock = document.createElement('p')
-
-      msgBlock.insertAdjacentHTML('beforeend', `<b>${payload.name}:</b> ${payload.body}`)
-      chatBox.appendChild(msgBlock)
-    })
+  init: socket => {
+    const channel = socket.channel('main:lobby', {});
+    channel.join();
+    listenForChats(channel);
   }
-}
+};
 
-export default DemoElixir
+export default DemoElixir;
